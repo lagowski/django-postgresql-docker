5e7419d834789c89cd7a5e66a051f654a212c112
Rafal Lagowski <lagowski@pro4.es>
2019-05-11 01:01:28 +0200

    Better start and make migrations

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-python manage.py migrate
+python manage.py makemigrations
 
+until python manage.py migrate; do
+  sleep 2
+  echo "Retry!";
+done
+
+
+python manage.py makemigrations
+python manage.py migrate
+echo "Django is ready.";
 python manage.py runserver 0.0.0.0:80
